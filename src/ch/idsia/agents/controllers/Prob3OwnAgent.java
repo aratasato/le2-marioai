467130bd17f73558900198d1e71c1d8ed86b7b7b
aratasato <14242642+aratasato@users.noreply.github.com>
2019-10-15 15:31:58 +0900

    課題3done

--- src/ch/idsia/agents/controllers/Prob3OwnAgent.java
+++ src/ch/idsia/agents/controllers/Prob3OwnAgent.java
@@ -27,35 +27,42 @@ public class Prob3OwnAgent extends BasicMarioAIAgent implements Agent {
                 || getReceptiveFieldCellValue(r, c) == GeneralizerLevelScene.LADDER;
     }
 
-    private boolean shouldAvoidGoomba() {
+    private boolean existsGoombaInFront(int offset) {
         int l = 10;
-        int r = 15;
+        int r = offset + l;
         for (int i = l; i <= r; i++) {
-            if (getEnemiesCellValue(marioEgoRow, i) == Sprite.KIND_GOOMBA
-                    || getEnemiesCellValue(marioEgoRow + 1, i) == Sprite.KIND_GOOMBA) {
-                return true;
+            for (int j = -1; j <= 3; j++) {
+                if (getEnemiesCellValue(marioEgoRow + j, i) == Sprite.KIND_GOOMBA) {
+                    return true;
+                }
             }
         }
         return false;
     }
 
     public boolean[] getAction() {
+        action[Mario.KEY_RIGHT] = true;
         action[Mario.KEY_SPEED] = false;
+
         if (isObstacle(marioEgoRow, marioEgoCol + 1) || isObstacle(marioEgoRow, marioEgoCol + 2)) {
             action[Mario.KEY_JUMP] = isMarioAbleToJump || !isMarioOnGround;
+            action[Mario.KEY_SPEED] = true;
         }
 
         if (!isObstacle(marioEgoRow - 3, marioEgoCol + 7) && isObstacle(marioEgoRow, marioEgoCol + 8)) {
             action[Mario.KEY_JUMP] = isMarioAbleToJump || !isMarioOnGround;
         }
 
-        if (shouldAvoidGoomba()) {
-            action[Mario.KEY_JUMP] = isMarioAbleToJump || !isMarioOnGround;
+        if (existsGoombaInFront(2) && isMarioOnGround) {
+            action[Mario.KEY_RIGHT] = false;
+            action[Mario.KEY_JUMP] = isMarioAbleToJump;
         }
 
-        if (isMarioAbleToShoot) {
+        if (existsGoombaInFront(6) && isMarioAbleToShoot) {
+            action[Mario.KEY_RIGHT] = false;
             action[Mario.KEY_SPEED] = true;
         }
+
 
         return action;
     }
