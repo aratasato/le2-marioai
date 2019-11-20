d2ba43d0854d762861e08099b4cbce0e7e9bf243
aratasato <14242642+aratasato@users.noreply.github.com>
2019-11-21 01:55:14 +0900

    learn 1

--- src/ch/idsia/scenarios/Prob1.java
+++ src/ch/idsia/scenarios/Prob1.java
@@ -1,27 +1,47 @@
 package ch.idsia.scenarios;
 
 import ch.idsia.agents.Agent;
+import ch.idsia.agents.AgentsPool;
+import ch.idsia.agents.LearningAgent;
+import ch.idsia.agents.LearningWithAS;
 import ch.idsia.agents.controllers.ForwardAgent;
 import ch.idsia.agents.controllers.ForwardJumpingAgent;
 import ch.idsia.benchmark.tasks.BasicTask;
 import ch.idsia.tools.MarioAIOptions;
 
 public final class Prob1 {
-	public static void main(String[] args) {
-		final MarioAIOptions marioAIOptions = new MarioAIOptions(args);
+    public static void main(String[] args) {
+//        Agent agent = new ForwardAgent();
 
-		int seed = 1;
-		int d = 0;
-		final Agent agent = new ForwardAgent();
+        AgentsPool.addAgent(AgentsPool.loadAgent("AS1_256.xml", false));
+        final Agent agent = AgentsPool.getCurrentAgent();
+        String aiOptionsArgs = "-ld 0 -ls 1";
 
-	    marioAIOptions.setAgent(agent);
-		marioAIOptions.setLevelRandSeed(seed);
-		marioAIOptions.setLevelDifficulty(d);
+        // learn
+//        learnWithAS(aiOptionsArgs, "AS1");
 
-		final BasicTask basicTask = new BasicTask(marioAIOptions);
-		basicTask.setOptionsAndReset(marioAIOptions);
-		basicTask.doEpisodes(1, true, 1);
-		System.exit(0);
-	}
+        // play
+        play(aiOptionsArgs, agent);
+    }
 
+    private static void play(String args, Agent agent) {
+        final MarioAIOptions marioAIOptions = new MarioAIOptions();
+
+        marioAIOptions.setAgent(agent);
+        marioAIOptions.setArgs(args);
+
+        final BasicTask basicTask = new BasicTask(marioAIOptions);
+        basicTask.setOptionsAndReset(marioAIOptions);
+        basicTask.doEpisodes(1, true, 1);
+        System.exit(0);
+    }
+
+    private static void learnWithAS(String args, String filename) {
+        LearningAgent learningAgent = new LearningWithAS(
+                args,
+                filename
+        );
+        learningAgent.learn();
+        System.exit(0);
+    }
 }
